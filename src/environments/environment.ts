f2978b677162352aef648edade75c9d901b591cd
George <tsiga.george@gmail.com>
2026-02-21 15:58:32 +0200

    Add files

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:44321/api/', // Development API URL
+  apiUrls: 'http://localhost:5000/api/' // Development API URL
+};
